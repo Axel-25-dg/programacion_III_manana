29876f8a963e6040753c65cac8eb68c433ef71fb
Axel DG <alexander18br17@gmail.com>
2025-10-21 10:03:45 -0500

    clase 2025-10-21

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <= 5; i++) {
+    console.log("Iteración número:", i);
+}
+
+let suma = 0;
+for (let i = 1; i <= 10; i++) {
+    suma += i;
+}
+console.log("La sumatoria es:", suma);
+
+let numero = 4;
+console.log("Tabla de multiplicar del", numero);
+for (let i = 1; i <= 10; i++) {
+    console.log(numero, "x", i, "=", numero * i);
+}
+
+let Nombre = "Alexander";
+for (let i = 0; i < Nombre.length; i++) {
+    console.log("Letra en posición", i, "es", Nombre[i]);
+}
+
+
+let nombre = "francisco";
+for (let i = 0; i < nombre.length; i++) {
+    console.log(nombre[i]);
+}
+
