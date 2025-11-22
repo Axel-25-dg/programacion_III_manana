b06fc58aea4975e472c1c5009f2bfbdf2cadf2e2
Axel DG <alexander18br17@gmail.com>
2025-11-22 17:01:24 -0500

    Deber 2025-11-04

--- modulo2/Typescript/06_interface_mp.js
+++ modulo2/Typescript/06_interface_mp.js
@@ -1,21 +1,18 @@
-import { Empleado } from "./05_interface_mp";
-
-const empleado1: Empleado = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var empleado1 = {
     id: 101,
     nombre: 'Alexander Ruiz'
 };
-
-const empleado2: Empleado = {
+var empleado2 = {
     id: 102,
     nombre: 'María Torres',
     correo: 'maria.torres@rrhh.com'
 };
-
 console.log(empleado1);
 console.log(empleado1.id);
 console.log(empleado1.nombre);
 console.log(empleado1.correo);
-
 console.log(empleado2);
 console.log(empleado2.id);
 console.log(empleado2.nombre);
