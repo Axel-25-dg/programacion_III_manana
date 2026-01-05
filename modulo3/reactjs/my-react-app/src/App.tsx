1ba15290a2c08d54a9a3732bf818fbb5c022009f
Axel DG <alexander18br17@gmail.com>
2026-01-05 08:21:25 -0500

    Clase

--- modulo3/reactjs/my-react-app/src/App.tsx
+++ modulo3/reactjs/my-react-app/src/App.tsx
@@ -1,36 +1,11 @@
-/*
-import CirculoColorido from "./useRef/CirculoColorido";
-import FocoAutomatico from "./useRef/focoAutomatico";
-import MoverCaja from "./useRef/MoverCaja";
-import ReproductorSonido from "./useRef/ReproducirSonido";
-import ScrollDemo from "./useRef/ScrollDemo";
+import TablaMultiplicar from "./UseCallBack/React";
+
 function App() {
   return (
     <div>
-      <FocoAutomatico />
-      <br />
-      <br />
-      <CirculoColorido />
-      <br />
-      <br />
-      <ScrollDemo />
-      <br />
-      <br />
-      <ReproductorSonido />
-      <br />
-      <br />
-      <MoverCaja />
+      <TablaMultiplicar />
     </div>
   );
 }
 export default App;
-*/
-import ExpensiveCalc from "./UseMemo/ExpensiveCalc";
-function App() {
-  return (
-    <div>
-      <ExpensiveCalc />
-    </div>
-  );
-}
-export default App;
+
